5a835b4e01845e5c02a8792c1b58fc0cadd4750e
Tabish Tanseef <tabishtanseef@gmail.com>
2017-10-28 22:54:57 +0530

    Created spy class

--- spy_details.py
+++ spy_details.py
@@ -1,7 +1,13 @@
-spy = {
-  'name': 'Tabish',
-  'salutation': 'Mr.',
-  'age': 24,
-  'rating': 4.7,
-  'is_online': True
-}
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+        # Initializing the values
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = None
+        # Count the number of words
+        self.count = 0
